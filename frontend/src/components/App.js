74a958711cbabb731b27e262dd8bd7cd1168e442
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-06 13:58:52 +0100

    add  form and leads components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,26 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './layout/Header';
+
+import Dashboard from './leads/Dashboard'
 class App extends Component {
 
 
     render() {
 
-        return <h1>hello react medo gueddou </h1>
+        return (
+
+    <Fragment>
+  <Header/>
+  <div className="container" >
+
+  <Dashboard/>
+  </div>
+ 
+</Fragment>
+          
+        )
     }
 }
 
