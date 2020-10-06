dcf35e0a1aa1906ad2cdae8eb1f43059ef0b7607
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-07 00:11:20 +0100

    display data using reactjs

--- frontend/src/components/leads/Leads.js
+++ frontend/src/components/leads/Leads.js
@@ -23,7 +23,35 @@ export class Leads extends Component {
     render() {
         return (
             <Fragment>
-               <h1>Leads List</h1> 
+               <h1>Leads</h1> 
+<table className=" table table-striped ">
+
+<thead>
+
+    <tr>
+
+        <th> ID </th>
+        <th> Name </th>
+        <th> Email </th>
+        <th> Message </th>
+         <th/>
+    </tr>
+</thead>
+<tbody>
+{this.props.leads.map(lead => (
+    <tr key={lead.id}>
+         <td>{lead.id} </td>
+         <td>{lead.name} </td>
+         <td>{lead.email} </td>
+         <td>{lead.message} </td>
+         <td>  <button className="btn btn-danger btn-sm"> 
+         Delete </button> </td>
+    </tr>
+))}
+
+</tbody>
+
+</table>
             </Fragment>
         )
     }
