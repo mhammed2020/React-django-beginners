6a09375ca6570dd5499d2e68c5fcbdf2def984f7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-06 13:32:03 +0100

    the first structure of the project

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+class App extends Component {
+
+
+    render() {
+
+        return <h1>hello react medo gueddou </h1>
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'));
