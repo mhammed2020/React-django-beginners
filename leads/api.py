279431ad64677a78cb92a8529e42d46eb6aad407
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-12 14:00:08 +0100

    add more data with authentication

--- leads/api.py
+++ leads/api.py
@@ -15,7 +15,7 @@ class LeadViewSet(viewsets.ModelViewSet) :
 
     serializer_class = LeadSerializer
     def get_queryset(self) :
-        return self.user.leads.all()
+        return self.request.user.leads.all()
 
     def perform_create(self,serializer):
         serializer.save(owner = self.request.user)
